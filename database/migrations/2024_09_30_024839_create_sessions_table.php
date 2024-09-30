38d7fcc3ad61995b6fc8378498e6eb0e5e79a04e
reiquan <reiquang@yahoo.com>
2024-09-29 22:25:20 -0700

    fixed errors in session table

--- database/migrations/2024_09_30_024839_create_sessions_table.php
+++ database/migrations/2024_09_30_024839_create_sessions_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInt('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('ip_address');
             $table->longText('payload');
+            $table->string('last_activity');
             $table->timestamps();
         });
     }
