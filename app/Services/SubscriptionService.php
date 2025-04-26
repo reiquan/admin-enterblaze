f27c42e95e480142301a4cdc98c7a9a7b80d4b97
reiquan <reiquang@yahoo.com>
2025-04-25 23:07:52 -0700

    fixed reservation number not populating on receipt

--- app/Services/SubscriptionService.php
+++ app/Services/SubscriptionService.php
@@ -194,6 +194,7 @@ class SubscriptionService
              return $alert_body;
 
          } else  if($type == 'reservation'){
+            
             $alert_body =  [ 
                  'alert_title' => 'Reservation made! ',
                  'alert_body' => 'Reservation #:'. $body['reservation_number'],
