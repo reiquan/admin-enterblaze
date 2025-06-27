cf1b31f6443b0addc05cbcb64e488538a3a35612
reiquan <reiquang@yahoo.com>
2025-06-27 01:45:45 -0700

    Make a waiting list in Admin with a button to approve payment

--- resources/views/events/registrations/show.blade.php
+++ resources/views/events/registrations/show.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-@if($event_registration->registration_type == 'Online Tournament')
+@if($event_registration->registration_type == 'Tournament')
   <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
           <!-- <a type="button" href="{{ route('events.create') }}" class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
@@ -40,6 +40,7 @@
                   <thead>
                     <tr>
                       <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Handle</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></th>
                       <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
 
                       <!-- <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
@@ -65,7 +66,7 @@
                         <td class="whitespace-nowrap px-3 py-4 text-sm text-green-500"></td>
                         @endif
                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          <select id="{{ $attendance->id }}status" onchange="changeParticipantStatus('{{ $attendance->id }}')" name="location" class="mt-2 block  rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm/6">
+                          <select id="{{ $attendance->id }}status" onchange="changeAttendeeStatus('{{ $attendance->id }}')" name="location" class="mt-2 block  rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm/6">
                             @if($attendance->attendee_status == 'Champion')
                               <option selected>Champion</option>
                               <option>Eliminated</option>
@@ -89,6 +90,81 @@
                 </table>
               @else
                 <h1> No events scheduled</h1>
+              @endif
+              </div>
+            </div>
+          </div>
+      </div>
+
+    </div>
+@elseif($event_registration->registration_type == 'Food Vendor' || $event_registration->registration_type == 'Vendor' || $event_registration->registration_type == 'Artist')
+  <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+          <!-- <a type="button" href="{{ route('events.create') }}" class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
+            </svg>
+            
+            
+            <span class="mt-2 block text-sm font-semibold text-gray-900">Create a new Attendee</span>
+          </a> -->
+        <div class="px-4 sm:px-6 lg:px-8">
+          <div class="mt-8 flow-root w-full">
+            <div class="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">        
+
+              @if(!empty($event_registration->attendances->toArray()))
+              <table class="min-w-full divide-y divide-gray-300">
+                  <thead>
+                    <tr>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Name</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Email</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Phone #</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Website</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
+                      <!-- <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                        <span class="sr-only">Edit</span>
+                      </th>
+                      <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                        <span class="sr-only">Delete</span>
+                      </th>
+                      <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                        <span class="sr-only">View</span>
+                      </th> -->
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-200">
+                    @foreach($event_registration->attendances as $attendance)
+                      <tr>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$attendance->attendee_first_name }} {{$attendance->attendee_last_name }}</td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$attendance->attendee_email }}</td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$attendance->attendee_phone_number ?? 'n/a'}}</td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $attendance->attendee_company_url }}</td>
+
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <select id="{{ $attendance->id }}status" onchange="changeAttendeeStatus('{{ $attendance->id }}')" name="location" class="mt-2 block  rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm/6">
+                            @if($attendance->attendee_status == 'Accepted')
+                              <option selected>Accepted</option>
+                              <option>Rejected</option>
+                              <option>Pending</option>
+                            @elseif($attendance->attendee_status == 'Rejected')
+                              <option>Accepted</option>
+                              <option selected>Rejected</option>
+                              <option>Pending</option>
+                            @else
+                              <option>Accepted</option>
+                              <option>Rejected</option>
+                              <option selected>Pending</option>
+                            @endif
+                          </select>
+                        </td>
+                      </tr>
+                    @endforeach
+
+                    <!-- More people... -->
+                  </tbody>
+                </table>
+              @else
+                <h1> No attendees scheduled for this event yet.</h1>
               @endif
               </div>
             </div>
@@ -156,13 +232,18 @@
 <script>
         // let event_id = ";
        
-        function changeParticipantStatus(attendance_id) {
+        function changeAttendeeStatus(attendance_id) {
             console.log('here');
             let registration_id = "{{ $event_registration->id }}";
             let event_id = "{{ $event_registration->event->id }}";
 
             // Display a confirmation dialog
-            var userConfirmed = window.confirm('Are you sure you want to change the status of ' + attendance_id + ' to ' + event.target.value + '?' );
+            if(event.target.value == 'Accepted'){
+              var userConfirmed = window.confirm("Are you sure you want to change the status of id number " + attendance_id + " to " + event.target.value + "? Doing So will charge the attendee's card on file. Proceed?" );
+            } else {
+              var userConfirmed = window.confirm('Are you sure you want to change the status of id number' + attendance_id + ' to ' + event.target.value + '?' );
+            }
+            
 
             // If the user clicks "OK" (true), redirect to another page
             if (userConfirmed) {
