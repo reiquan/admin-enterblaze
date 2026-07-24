1c98482da4bd5527588425e1bff82b4f248b265f
reiquan <reiquang@yahoo.com>
2026-07-23 19:02:01 -0700

    fixed book creation with validation

--- resources/views/livewire/book-cover-upload.blade.php
+++ resources/views/livewire/book-cover-upload.blade.php
@@ -9,14 +9,23 @@
     <div class="aspect-[4/5] w-full rounded-3xl border-2 border-dashed border-gray-300 bg-gray-50"></div>
 @endif
 
-        <input type="file" wire:model="photo">
+        <input
+            type="file"
+            wire:model="photo"
+            accept="image/*"
+        >
 
        
     
-        @error('photo') <span class="error">{{ $message }}</span> @enderror
+
     
         @if(isset($logo))
-            <button class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"  wire:click="saveBookCover">Replace Photo</button>
+            @error('photo')
+                <p class="mt-2 text-sm text-red-600">
+                    {{ $message }}
+                </p>
+            @enderror
+            <button class="mt-6 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"  type="button" wire:click="saveBookCover">Update Photo</button>
         @else
             <button class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" wire:click="saveBookCover">Save Photo</button>
         @endif
